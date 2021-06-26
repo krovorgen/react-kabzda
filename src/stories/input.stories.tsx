224e0d175956d0dfd1e403501ac90d59d4a761b5
Максим Сергеевич <krovorgen@gmail.com>
2021-06-26 20:42:55 +0300

    added input.stories.tsx in storybook

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, useRef, useState } from 'react';
 import { Meta } from '@storybook/react';
 
 export default {
@@ -6,4 +6,31 @@ export default {
 } as Meta;
 
 export const UncontrolledInput = () => <input type="text" />;
-export const ControlledInput = () => <input type="text" value={'maksim'} />;
+
+export const TrackValueOfUncontrolledInput = () => {
+    const [value, setValue] = useState<string>('');
+    const valueChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value);
+    };
+    return (
+        <>
+            <input type="text" value={value} onChange={valueChange} /> - {value}
+        </>
+    );
+};
+
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const [value, setValue] = useState<string>('');
+    const inputRef = useRef<HTMLInputElement>(null);
+    const onSave = () => {
+        const el = inputRef.current as HTMLInputElement;
+        setValue(el.value);
+    };
+    return (
+        <>
+            <input type="text" ref={inputRef} /> / <button onClick={onSave}>save</button> - {value}
+        </>
+    );
+};
+
+export const ControlledInputWithFixedValue = () => <input type="text" value={'maksim'} />;
